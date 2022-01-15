6d7ec7b7cb85672fa3a70b4230f79d6f670e480e
pickman murimi <pickmanmurimi@gmail.com>
2022-01-15 18:31:51 +0300

    Lumen Setup

---
+++ README.md
@@ -0,0 +1,4 @@
+# Senior Software Engineer - Mini Project (NodeJS/PHP)
+# Lumen
+
+## setup
---
+++ routes/web.php
@@ -0,0 +1,38 @@
+<?php
+
+/** @var \Laravel\Lumen\Routing\Router $router */
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+$router->get('/', function () use ($router) {
+    return $router->app->version();
+});
+
+//api/v1
+    //auth
+        //login
+        //register
+    //users
+        //new
+        //show
+        //update/:id
+        //delete/:id
+    //roles
+        //new
+        //show
+        //update/:id
+        //delete/:id
+    //products
+        //new
+        //show
+        //update/:id
+        //delete/:id
